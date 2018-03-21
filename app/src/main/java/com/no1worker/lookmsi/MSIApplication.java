0c7157e334663342a32a468ef9eb77029a47097f
yuzhiyong <741347146@qq.com>
2018-03-21 22:05:19 +0800

    程序主Application，未在XML注册

---
+++ app/src/main/java/com/no1worker/lookmsi/MSIApplication.java
@@ -0,0 +1,11 @@
+package com.no1worker.lookmsi;
+
+import android.app.Application;
+
+/**
+ * 主程序 Application
+ * Created by yuzhiyong on 2018/3/21.
+ */
+
+public class MSIApplication extends Application{
+}
