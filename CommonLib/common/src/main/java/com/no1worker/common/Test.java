ceb755a206ec76f7aff0eb8eb87ed24d3065897b
yuzhiyong <741347146@qq.com>
2018-03-26 18:18:14 +0800

    增加common模块，创建CommonActivity

---
+++ CommonLib/common/src/main/java/com/no1worker/common/Test.java
@@ -0,0 +1,20 @@
+package com.no1worker.common;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import com.no1worker.common.activity.CommonActivity;
+
+/**
+ * Created by yuzhiyong on 2018/3/26.
+ */
+public class Test extends CommonActivity {
+
+    @Override
+    public void findView() {}
+
+    @Override
+    public void initView() {}
+
+    @Override
+    public void bindListener() {}
+}
---
+++ CommonLib/common/src/main/java/com/no1worker/common/activity/CommonActivity.java
@@ -0,0 +1,95 @@
+package com.no1worker.common.activity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import org.greenrobot.eventbus.EventBus;
+
+/**
+ * Created by yuzhiyong on 2018/3/26.
+ */
+public abstract class CommonActivity extends Activity {
+
+    public Activity activity;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activity = this;
+        /**
+         * activity被回收之后，下次启动时清除上次保存的fragment。
+         */
+        if (savedInstanceState != null) {
+            savedInstanceState.putParcelable("android:support:fragments", null);
+        }
+        registerEventBus();
+        findView();
+        beforeInit();
+        initView();
+        afterInit();
+        bindListener();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterEventBus();
+    }
+
+    public abstract void findView();
+
+    public abstract void initView();
+
+    public abstract void bindListener();
+
+    /**
+     * 在initView之前进行一点操作，可不重写
+     */
+    public void beforeInit() {}
+
+    /**
+     * 在initView之后进行一点操作，可不重写
+     */
+    public void afterInit() {}
+
+    /**
+     * 注册EventBus
+     */
+    public void registerEventBus() {
+        EventBus.getDefault().register(this);
+    }
+
+    /**
+     * 反注册EventBus
+     */
+    private void unregisterEventBus() {
+        EventBus.getDefault().unregister(this);
+    }
+
+    /**
+     * 隐藏键盘
+     */
+    protected void hideSoftInputMethod() {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(
+            Context.INPUT_METHOD_SERVICE);
+        if (getCurrentFocus() != null) {
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+    }
+
+    /**
+     * 显示键盘
+     *
+     * @param view
+     */
+    protected void showSoftInputMethod(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(
+            Context.INPUT_METHOD_SERVICE);
+        // 接受软键盘输入的编辑文本或其它视图
+        inputMethodManager.showSoftInput(view, InputMethodManager.SHOW_FORCED);
+    }
+}
